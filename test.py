f307755cde363d587fa82939478e0e9d9f533985
DjangoOne <elias@sayodev.de>
2016-01-12 19:55:54 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 """
-test.py
-
 Ein kleines Test-Programm für die kleine LED-Stripe-Library.
-Wenn alles glatt läuft, müsste die Lichterkette von rot über lila bis blau erleuchten.
+Wenn alles glatt läuft, müsste die Lichterkette komplett in rot erleuchten.
 
 © Elias Gürlich, 2016
 """
@@ -10,23 +8,20 @@ Wenn alles glatt läuft, müsste die Lichterkette von rot über lila bis blau er
 from led_stripe_py import LED_Stripe, LED_Stripe_Datagram, LED # Alles aus der LED-Stripe-Lib importieren
 import math # Importiere natives Math-Modul
 
-HOST_NAME = "localhost"
+HOST_NAME = "172.22.99.206"
 PORT = 2342
 
 led_stripe = LED_Stripe(HOST_NAME, PORT) # Erzeuge Lichterkette mit diesem Host und Port
 
 leds = [] # Hier kommen die LEDs rein *-*
-for i in range(0, 103): # Mache 103 mal...
-    led = LED(0xff, 0x00, math.ceil(0x00 + (0xff/103) * i)) # Fange bei ganz rot an und werde immer lilaner
-    leds.append(led) # Füge LED hinzu
+for i in range(0,226): #Für 226 LEDs
+    led = LED(0xff, 0x00, 0x00) #Setze LED rot
+    leds.append(led) 
 
-for i in range(0, 103): # Mache nochmal 103 mal...
-    led = LED(math.ceil(0xff - (0xff/103) * i), 0x00, 0xff) # Fange bei ganz lila an und werde immer blauer
-    leds.append(led) # Füge LED hinzu
+datagram = LED_Stripe_Datagram(priority=0x01, command=0x00, leds=leds) # Erzeuge Datagram mit Priorität 255, (seeeeehr hoch) Kommando 0 (Setze LED-Farben) und den ganzen LEDs, die wir definiert haben
 
-datagram = LED_Stripe_Datagram(priority=0xff, command=0x00, leds=leds) # Erzeuge Datagram mit Priorität 255, (seeeeehr hoch) Kommando 0 (Setze LED-Farben) und den ganzen LEDs, die wir definiert haben
-
-led_stripe.send(datagram) # Sende Datagram an Lichterkette
+while True:
+    led_stripe.send(datagram) # Sende Datagram an Lichterkette
 
 # Wenn alles glatt gelaufen ist, sollte man jetzt oben beschriebenes Phänomen beobachten können.
 # Falls dies aber nicht der Fall sein sollte, dann funktioniert dieses Test-Programm offensichtlich nicht einwandfrei. :(
